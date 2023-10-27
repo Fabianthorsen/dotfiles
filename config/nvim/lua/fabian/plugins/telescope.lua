8af5d83e8d6d9fb6c1baab3ae5d47f35133e09e5
Fabian Thorsen <thorsen.fabian@gmail.com>
2023-10-27 21:30:27 +0200

    adding more lsp config

--- config/nvim/lua/fabian/plugins/telescope.lua
+++ config/nvim/lua/fabian/plugins/telescope.lua
@@ -1,15 +1,22 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  tag = "0.1.4",
-  dependencies = { 'nvim-lua/plenary.nvim' },
-  config = function ()
-    local builtin = require("telescope.builtin")
-    local keymap = vim.keymap
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.4",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local telescope = require("telescope")
+		local builtin = require("telescope.builtin")
+		local keymap = vim.keymap
 
-    keymap.set("n", "<leader>pf", builtin.find_files, {})
-    keymap.set("n", "<C-p>", builtin.git_files, {})
-    keymap.set("n", "<leader>ps",function ()
-      builtin.grep_string({ search = vim.fn.input("Grep > ") })
-    end, {})
-  end
+		keymap.set("n", "<leader>pf", builtin.find_files, {})
+		keymap.set("n", "<C-p>", builtin.git_files, {})
+		keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end, {})
+
+		telescope.load_extension("fzf")
+	end,
 }

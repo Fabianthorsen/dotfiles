fc4763726fd7c853eaa05da6744516b593aa2970
Fabian Thorsen <fabian.thorsen@statnett.no>
2023-10-27 14:42:05 +0200

    add formatters and ignore

---
+++ config/nvim/lua/fabian/plugins/formatting.lua
@@ -0,0 +1,31 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local conform = require("conform")
+		local keymap = vim.keymap
+		conform.setup({
+			formatters_by_ft = {
+				python = { "isort", "black" },
+				terraform = { "terraform_fmt" },
+				yaml = { "prettier" },
+				json = { "prettier" },
+				markdown = { "prettier" },
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			},
+		})
+
+		keymap.set({ "n", "v" }, "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 500,
+			})
+		end)
+	end,
+}

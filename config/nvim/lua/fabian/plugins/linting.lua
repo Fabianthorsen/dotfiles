faa38d8408e05790f165f96beaef08132e92eec6
Fabian Thorsen <fabian.thorsen@statnett.no>
2023-10-30 08:35:00 +0100

    add linter and update alacritty

---
+++ config/nvim/lua/fabian/plugins/linting.lua
@@ -0,0 +1,10 @@
+return {
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
+		lint.linter_by_ft = {
+			"ruff",
+		}
+	end,
+}
